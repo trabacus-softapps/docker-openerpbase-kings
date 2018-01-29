0282fe67d275f887d9eda97e07d253470ac01ffb
Arun T K <arun.kalikeri@softappsit.com>
2018-01-29 17:16:40 +0530

    added google hedless insted of phantomjs

--- finalize.sh
+++ finalize.sh
@@ -4,7 +4,7 @@ source /pd_build/buildconfig
 
 header "Finalizing..."
 
-run apt-get autoremove
+run apt-get -y autoremove
 run apt-get clean
 run rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 run rm -rf /pd_build
